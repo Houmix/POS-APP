abc0064535f476669e8410e3a481eaa25bbd7ad5
Houmix <lachache.houmame@outlook.com>
2026-03-11 23:27:02 +0100

    for images

--- born_dz/born_dz/urls.py
+++ born_dz/born_dz/urls.py
@@ -41,6 +41,6 @@ urlpatterns = [
     # Kiosk config
     path('api/kiosk/config/', KioskConfigView.as_view(), name='kiosk_config'),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
