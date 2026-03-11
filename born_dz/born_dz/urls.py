c4f6e3175ac781e6bc9fceb29a533d813610f776
Houmix <lachache.houmame@outlook.com>
2026-03-11 23:35:32 +0100

    Update urls.py

--- born_dz/born_dz/urls.py
+++ born_dz/born_dz/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 from restaurant.views import KioskConfigView
 
 urlpatterns = [
@@ -41,6 +42,8 @@ urlpatterns = [
     # Kiosk config
     path('api/kiosk/config/', KioskConfigView.as_view(), name='kiosk_config'),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
