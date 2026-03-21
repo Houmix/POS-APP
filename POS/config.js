272561bad0b4e5e7cfddc829341d90b21816e2b1
Houmix <lachache.houmame@outlook.com>
2026-03-21 02:45:09 +0100

    Update config.js

--- POS/config.js
+++ POS/config.js
@@ -1,13 +1,3 @@
 // config.js
-// URL du serveur chargée dynamiquement depuis AsyncStorage.
-// Utiliser getPosUrl() dans vos composants/hooks.
-import { getPosUrl } from './utils/serverConfig';
-
-export const idRestaurant = 1;
-
-// Pour compatibilité : utilise getPosUrl() partout à la place
-export { getPosUrl as getPosUrl };
-
-// DEPRECATED - ne plus utiliser POS_URL directement
-// L'URL est maintenant dynamique, utilisez getPosUrl()
-export const POS_URL = 'http://127.0.0.1:8000';
+// Utiliser getPosUrl() et getRestaurantId() depuis utils/serverConfig partout.
+export { getPosUrl, getRestaurantId } from './utils/serverConfig';
